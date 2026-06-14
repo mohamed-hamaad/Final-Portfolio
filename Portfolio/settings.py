09fd7ead89857eebd2a8cfaaacbda8f2ba556756
Mohamed Hamaad <mohamednaderhamaad@gmail.com>
2026-06-15 00:22:54 +0300

    Update settings.py to use PostgreSQL database

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -82,11 +82,10 @@ WSGI_APPLICATION = 'Portfolio.wsgi.application'
 
 
 
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join('/tmp', 'db.sqlite3') if os.environ.get('VERCEL') else BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgresql://neondb_owner:npg_GCS1lTyvx8kM@ep-polished-fog-at3zarnk.c-9.us-east-1.aws.neon.tech/neondb?sslmode=require')
 }
 
 
